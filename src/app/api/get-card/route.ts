903ee68dc216bf5b04e17ce6379359867698d7e6
Etheirystech <tofuking5@gmail.com>
2023-12-07 15:03:11 +0100

    sixth commit

--- src/app/api/get-card/route.ts
+++ src/app/api/get-card/route.ts
@@ -10,7 +10,8 @@ export async function GET() {
 
 
     // Launch Puppeteer
-    const browser = await puppeteer.launch({headless: "new"});
+    const browser = await puppeteer.launch({headless: "new",
+        args: ["--no-sandbox"],});
     const page = await browser.newPage();
 
     await page.goto(
